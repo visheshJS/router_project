aadca8445192f1395a87c3fc8ec6c0b1098af391
visheshJS <visheshsharma_23me287@dtu.ac.in>
2025-01-15 00:46:27 +0530

    Fixed header import issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header/header'
-import Footer from './components/footer/footer'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 
 
