035ff05b81f974a6092f261b787ea9ecef4cb60b
visheshJS <visheshsharma_23me287@dtu.ac.in>
2025-01-15 01:01:12 +0530

    Renamed 'header' to 'temp'

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header/Header'
+import Header from './components/header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 
